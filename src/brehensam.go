23876d565293e42dd7fd49c0fe1d5a6eb9847e93
johntgz <john_tanguanzhong@hotmail.com>
2021-12-19 21:58:02 +0800

    finished brehensam float and int functions

--- src/brehensam.go
+++ src/brehensam.go
@@ -49,8 +49,10 @@ func getInput(filepath string) ([][]int, int, int) {
 	//read file line by line
 	for scanner.Scan() {
 		text := scanner.Text()
+		//all relevant data end at ";"
+		test_processed := strings.Split(text, ";")
 
-		coordinates := strings.Split(text, " -> ")
+		coordinates := strings.Split(test_processed[0], " -> ")
 		xy1 := strings.Split(coordinates[0], ",")
 		xy2 := strings.Split(coordinates[1], ",")
 
@@ -60,8 +62,14 @@ func getInput(filepath string) ([][]int, int, int) {
 		y2, _ := strconv.Atoi(xy2[1])
 
 		//Get max width and height: this code assumes non-negative coordinate values
-		max_width = line_render.Max(x1, x2)
-		max_height = line_render.Max(y1, y2)
+		max_x := line_render.Max(x1, x2)
+		max_y := line_render.Max(y1, y2)
+		if max_width < max_x {
+			max_width = max_x
+		}
+		if max_height < max_y {
+			max_height = max_y
+		}
 
 		xy_pos := []int{x1, y1, x2, y2}
 
@@ -71,10 +79,9 @@ func getInput(filepath string) ([][]int, int, int) {
 	return coordinate_arr, max_width + 1, max_height + 1
 }
 
-func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
+func brehensamFloat(img *image.RGBA, coords []int, color color.RGBA) {
 	x1, y1, x2, y2 := coords[0], coords[1], coords[2], coords[3]
 	dx, dy := x2-x1, y2-y1
-	fmt.Printf("dx: %d, dy: %d \n", dx, dy)
 
 	gradient_case := 0
 
@@ -82,9 +89,9 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 		fmt.Printf("Start and end coordinates are the same \n")
 		return
 	} else if dx == 0 { // m == INF
-		gradient_case = 9
+		gradient_case = 5
 	} else if dy == 0 { // m == 0
-		gradient_case = 10
+		gradient_case = 6
 	} else if dy > 0 { // m > 0
 		if dx > 0 { //1st Quadrant, Top right
 			if line_render.Abs(dy) > line_render.Abs(dx) { //1st Octant ( 1 < m < INF )
@@ -92,31 +99,31 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 			} else { //2nd Octant ( 0 < m <= 1 )
 				gradient_case = 2
 			}
-		} else { //3rd Quadrant, Btm Left
-			if line_render.Abs(dy) > line_render.Abs(dx) { //5th Octant ( 1 < m < INF )
-				gradient_case = 5
-			} else { //6th Octant ( 0 < m <= 1 )
-				gradient_case = 6
+		} else { //4th Quadrant, Top left
+			if line_render.Abs(dy) > line_render.Abs(dx) { //8th Octant ( -INF < m < -1 )
+				gradient_case = 4
+			} else { //7th Octant ( -1 <= m < 0 )
+				gradient_case = 3
 			}
 		}
-	} else if dy < 0 { // Negative gradient
+	} else if dy < 0 { // m < 0
 		if dx > 0 { //2nd Quadrant, Btm right
 			if line_render.Abs(dy) > line_render.Abs(dx) { //4th Octant ( -INF < m < -1 )
 				gradient_case = 4
 			} else { //3rd Octant ( -1 <= m < 0 )
 				gradient_case = 3
 			}
-		} else { //4th Quadrant, Top Left
-			if line_render.Abs(dy) > line_render.Abs(dx) { //8th Octant ( -INF < m < -1 )
-				gradient_case = 8
-			} else { //7th Octant ( -1 <= m < 0 )
-				gradient_case = 7
+		} else { //3rd Quadrant, Btm Left
+			if line_render.Abs(dy) > line_render.Abs(dx) { //5th Octant ( 1 < m < INF )
+				gradient_case = 1
+			} else { //6th Octant ( 0 < m <= 1 )
+				gradient_case = 2
 			}
 		}
 	}
 
 	var m float64
-	if gradient_case != 9 { //Only calculate gradient if it is not infinite
+	if gradient_case != 5 { //Only calculate gradient if it is not infinite
 		m = (float64(dy)) / (float64(dx)) //gradient
 	}
 
@@ -128,9 +135,9 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 	y_inc := line_render.CopySignInt(1, dy)
 
 	switch gradient_case {
-	case 1: //Quad 1: 1 < m < INF
+	case 1: //Octant 1 and 5: 1 < m < INF
 		fmt.Printf("1st Octant: 1 < m < INF \n")
-		for x, y, err := x1, y1, 0.0; y != y2+1; y += y_inc {
+		for x, y, err := x1, y1, 0.0; y != y2+y_inc; y += y_inc {
 			img.Set(x, y, color)
 			if (err + m) < 0.5 {
 				err += m
@@ -139,10 +146,9 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 				x += x_inc
 			}
 		}
-	case 2: //Quad 2: 0 < m <= 1
+	case 2: //Octant 2 and 6: 0 < m <= 1
 		fmt.Printf("2nd Octant: 0 < m <= 1 \n")
-		for x, y, err := x1, y1, 0.0; x != x2+1; x += x_inc {
-			fmt.Printf("2nd: %d, %d \n", x, y)
+		for x, y, err := x1, y1, 0.0; x != x2+x_inc; x += x_inc {
 			img.Set(x, y, color)
 			if (err + m) < 0.5 {
 				err += m
@@ -151,9 +157,9 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 				y += y_inc
 			}
 		}
-	case 3: //Quad 3: -1 <= m < 0
+	case 3: //Octant 3 and 7: -1 <= m < 0
 		fmt.Printf("3rd Octant: -1 <= m < 0 \n")
-		for x, y, err := x1, y1, 0.0; x != x2+1; x += x_inc {
+		for x, y, err := x1, y1, 0.0; x != x2+x_inc; x += x_inc {
 			img.Set(x, y, color)
 			if (err + m) > -0.5 {
 				err += m
@@ -162,9 +168,9 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 				y += y_inc
 			}
 		}
-	case 4: //Quad 4: -INF < m < -1
+	case 4: //Octant 4 and 8: -INF < m < -1
 		fmt.Printf("4th Octant: -INF < m < -1 \n")
-		for x, y, err := x1, y1, 0.0; y != y2-1; y += y_inc {
+		for x, y, err := x1, y1, 0.0; y != y2+y_inc; y += y_inc {
 			img.Set(x, y, color)
 			if (err + m) > -0.5 {
 				err += m
@@ -173,72 +179,15 @@ func brehensamFloat64(img *image.RGBA, coords []int, color color.RGBA) {
 				x += x_inc
 			}
 		}
-	case 5: //Quad 5:  1 < m < INF
-		fmt.Printf("5th Octant: 1 < m < INF \n")
-		for x, y, err := x1, y1, 0.0; y != y2-1; y += y_inc {
-			img.Set(x, y, color)
-			if (err + m) < 0.5 {
-				err += m
-			} else {
-				err += m - 1
-				x += x_inc
-			}
+	case 5: // m == INF
+		fmt.Printf("Vertical Line: m == INF \n")
+		for y := y1; y != y2+y_inc; y += y_inc {
+			img.Set(x1, y, color)
 		}
-	case 6: //Quad 6:  0 < m <= 1
-		fmt.Printf("6th Octant: 0 < m <= 1 \n")
-		for x, y, err := x1, y1, 0.0; x != x2-1; x += x_inc {
-			img.Set(x, y, color)
-			if (err + m) < 0.5 {
-				err += m
-			} else {
-				err += m - 1
-				y += y_inc
-			}
-		}
-	case 7: //Quad 7:  -1 <= m < 0
-		fmt.Printf("7th Octant: -1 <= m < 0 \n")
-		for x, y, err := x1, y1, 0.0; x != x2-1; x += x_inc {
-			img.Set(x, y, color)
-			if (err + m) > -0.5 {
-				err += m
-			} else {
-				err += m + 1
-				y += y_inc
-			}
-		}
-	case 8: //Quad 8:  -INF < m < -1
-		fmt.Printf("8th Octant: -INF < m < -1 \n")
-		for x, y, err := x1, y1, 0.0; y != y2+1; y += y_inc {
-			img.Set(x, y, color)
-			if (err + m) > -0.5 {
-				err += m
-			} else {
-				err += m + 1
-				x += x_inc
-			}
-		}
-	case 9: // m == INF
-		fmt.Printf("m == INF \n")
-		if dy > 0 { //Case 9a: Increasing y
-			for y := y1; y <= y2; y++ {
-				img.Set(x1, y, color)
-			}
-		} else { //Case 9b: Decreasing y
-			for y := y1; y >= y2; y-- {
-				img.Set(x1, y, color)
-			}
-		}
-	case 10: // m == 0
-		fmt.Printf(" m == 0 \n")
-		if dx > 0 { //Case 10a: Increasing x
-			for x := x1; x <= x2; x++ {
-				img.Set(x, y1, color)
-			}
-		} else { //Case 10b: Decreasing x
-
-			for x := x1; x >= x2; x-- {
-				img.Set(x, y1, color)
-			}
+	case 6: // m == 0
+		fmt.Printf("Horizontal Line: m == 0 \n")
+		for x := x1; x != x2+x_inc; x += x_inc {
+			img.Set(x, y1, color)
 		}
 
 	}
@@ -249,20 +198,49 @@ func brehensamInt(img *image.RGBA, coords []int, color color.RGBA) {
 	x1, y1, x2, y2 := coords[0], coords[1], coords[2], coords[3]
 	dx, dy := x2-x1, y2-y1
 
-	//error (fluctutates between -0.5 and 0.5)
-	err := 0
+	grad_sign := 1                    //Sign of gradient
+	if bool(dx < 0) != bool(dy < 0) { // (dx is negative) XOR (dy is negative)
+		grad_sign = -1
+	}
+	x_inc := line_render.CopySignInt(1, dx)
+	y_inc := line_render.CopySignInt(1, dy)
 
-	if dx > 0 {
-		x, y := x1, y1
-		for x <= x2 {
+	if dx == 0 && dy == 0 {
+		fmt.Printf("Start and end coordinates are the same \n")
+		return
+	} else if dx == 0 { // m == INF
+		// fmt.Printf("Vertical Line: m == INF \n")
+		for y := y1; y != y2+y_inc; y += y_inc {
+			img.Set(x1, y, color)
+		}
+	} else if dy == 0 { // m == 0
+		// fmt.Printf("Horizontal Line: m == 0 \n")
+		for x := x1; x != x2+x_inc; x += x_inc {
+			img.Set(x, y1, color)
+		}
+	} else if line_render.Abs(dy) > line_render.Abs(dx) { // 1 < m < INF
+		// fmt.Printf("1st, 5th, 4th and 8th Octant: 1 < m < INF \n")
+		for x, y, err := x1, y1, 0; y != y2+y_inc; y += y_inc {
+			// fmt.Printf("(%d, %d), err: %d \n", x, y, err)
 			img.Set(x, y, color)
-			if 2*(err+dy) < dx {
-				err += dy
+			if grad_sign*2*(err+dx) < dy {
+				err += y_inc * (dx)
 			} else {
-				err += dy - dx
-				y++
+				err += y_inc * (dx - (grad_sign * dy))
+				x += x_inc
 			}
-			x++
+		}
+	} else { // 0 < m <= 1
+		// fmt.Printf("2nd, 6th, 3rd and 7th Octant: 0 < m <= 1 \n")
+		for x, y, err := x1, y1, 0; x != x2+x_inc; x += x_inc {
+			// fmt.Printf("(%d, %d), err: %d \n", x, y, err)
+			img.Set(x, y, color)
+			if grad_sign*2*(err+dy) < dx {
+				err += x_inc * (dy)
+			} else {
+				err += x_inc * (dy - (grad_sign * dx))
+				y += y_inc
+			}
 		}
 	}
 
@@ -277,18 +255,18 @@ func main() {
 
 	coordinate_arr, max_width, max_height := getInput(input_file)
 
-	fmt.Printf("Width,Height: %d, %d \n", max_width, max_height)
+	fmt.Printf("Image Width,Height: %d, %d \n", max_width, max_height)
 
 	topLeft := image.Point{0, 0}
-	btmRight := image.Point{9, 9}
+	btmRight := image.Point{max_width, max_height}
 
 	img := image.NewRGBA(image.Rectangle{topLeft, btmRight})
 	cyan := color.RGBA{100, 200, 200, 0xff}
 
 	//iterate through each coord (x1, y1, x2, y2, dx, dy)
 	for _, coord := range coordinate_arr {
-		brehensamFloat64(img, coord, cyan)
-
+		// brehensamFloat(img, coord, cyan)
+		brehensamInt(img, coord, cyan)
 		// char, _, err := consoleReader.ReadRune()
 		// line_render.CheckErr(err)
 		// fmt.Printf("Current input: %s", char)
